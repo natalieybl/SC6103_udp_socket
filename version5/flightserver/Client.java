21d3a03935b71c63fa4ef564397a630b171cf365
natalieybl <94332856+natalieybl@users.noreply.github.com>
2024-10-07 12:21:26 +0000

    update ipaddress

--- version5/flightserver/Client.java
+++ version5/flightserver/Client.java
@@ -6,7 +6,7 @@ import java.util.InputMismatchException;
 
 
 public class Client {
-    private static final String SERVER_ADDRESS = "localhost";
+    private static final String SERVER_ADDRESS = "192.168.56.1";
     //private static final String SERVER_ADDRESS = "0.tcp.ap.ngrok.io";
 
     private static final int SERVER_PORT = 12345;
